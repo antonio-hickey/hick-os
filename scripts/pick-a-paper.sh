0189832e33b7002ef9301f557c753c2097db37cc
Antonio Hickey <contact@antoniohickey.com>
2025-04-27 20:34:29 -0400

    scripts(pick-a-paper): script to pick a wallpaper

---
+++ scripts/pick-a-paper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MONITOR="DP-2"
+
+WALLPAPER_DIR="$HOME/hick-os/wallpapers"
+
+SELECTED=$(find "$WALLPAPER_DIR" -type f | wofi --dmenu --prompt="Select Wallpaper: ")
+
+[ -z "$SELECTED" ] && exit
+
+hyprctl hyprpaper unload all
+hyprctl hyprpaper preload "$SELECTED"
+hyprctl hyprpaper wallpaper "$MONITOR,$SELECTED"
