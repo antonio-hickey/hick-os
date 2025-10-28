58885c4e5f2521bdf27b0891e263e970b3258530
Antonio Hickey <contact@antoniohickey.com>
2025-10-28 12:56:20 -0400

    feat(scripts): start tmux script

---
+++ scripts/start-tmux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SESSION="main"
+
+# Only create session if it doesn't exist
+tmux has-session -t $SESSION 2>/dev/null || {
+    tmux new-session -d -s $SESSION
+
+    # Create a big main left window, with 2 small 
+    # vertical split windows to the right.
+    #
+    # +-----------+----+
+    # |           |    | 
+    # |           +----+
+    # |           |    | 
+    # +-----------+----+
+    tmux split-window -h -p 25
+    tmux select-pane -R -t $SESSION:0.0
+    tmux split-window -v -p 50
+    tmux select-pane -t 0
+}
+
+tmux attach -t $SESSION
+
