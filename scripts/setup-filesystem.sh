eed92ff18e2dba3203165c48289b703dadae29b8
Antonio Hickey <contact@antoniohickey.com>
2025-10-28 17:08:05 -0400

    feat(scripts): setup base filesystem

---
+++ scripts/setup-filesystem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the base filesystem
+mkdir -p "$HOME/school"
+mkdir -p "$HOME/fun"
+mkdir -p "$HOME/work"
+mkdir -p "$HOME/art"
+mkdir -p "$HOME/tmp"
+mkdir -p "$HOME/tmp/build"
