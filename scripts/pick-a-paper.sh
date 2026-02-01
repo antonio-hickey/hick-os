9cac2ae28374b885fe3d09d1c2f1fb731732e4e4
Antonio Hickey <contact@antoniohickey.com>
2026-02-01 16:20:10 -0500

    fix(scripts): wallpaper picker to use new hypr protocol

--- scripts/pick-a-paper.sh
+++ scripts/pick-a-paper.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Main (center) monitor
+#
+# TODO: Define my other monitors in case I want to use in le future.
 MONITOR="DP-2"
 
+# The directory storing all my wallpapers
 WALLPAPER_DIR="$HOME/.hick-os/wallpapers"
 
+# Use wofi launcher to display a list of all wallpapers and select one.
 SELECTED=$(find "$WALLPAPER_DIR" -type f | wofi --dmenu --prompt="Select Wallpaper: ")
-
 [ -z "$SELECTED" ] && exit
 
-hyprctl hyprpaper unload all
-hyprctl hyprpaper preload "$SELECTED"
-hyprctl hyprpaper wallpaper "$MONITOR,$SELECTED"
+# Update the current wallpaper for the main monitor.
+#
+# TODO: Think about how to best handle my other monitors.
+hyprctl hyprpaper wallpaper "$MONITOR, $SELECTED, cover"
